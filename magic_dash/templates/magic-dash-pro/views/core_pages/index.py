7aab3e259d9d5a03f2482975dcdade4af19dd3df
CNFeffery <fefferypzy@gmail.com>
2024-12-31 14:28:38 +0800

    chore: 微调示例首页内容

--- magic_dash/templates/magic-dash-pro/views/core_pages/index.py
+++ magic_dash/templates/magic-dash-pro/views/core_pages/index.py
@@ -1,12 +1,20 @@
 import feffery_antd_components as fac
+from feffery_dash_utils.style_utils import style
 
 
 def render():
     """子页面：首页渲染简单示例"""
 
-    return fac.AntdAlert(
-        type="info",
-        showIcon=True,
-        message="欢迎来到首页！",
-        description="这里以首页为例，演示核心页面下，各子页面构建方式的简单示例😉~",
+    return fac.AntdSpace(
+        [
+            fac.AntdBreadcrumb(items=[{"title": "主要页面"}, {"title": "首页"}]),
+            fac.AntdAlert(
+                type="info",
+                showIcon=True,
+                message="欢迎来到首页！",
+                description="这里以首页为例，演示核心页面下，各子页面构建方式的简单示例😉~",
+            ),
+        ],
+        direction="vertical",
+        style=style(width="100%"),
     )
