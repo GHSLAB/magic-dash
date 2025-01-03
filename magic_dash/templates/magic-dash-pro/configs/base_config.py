aef065e3901b6c31c393f26c46f14e775b44e4d4
CNFeffery <fefferypzy@gmail.com>
2025-01-03 14:43:26 +0800

    feat: magic-dash-pro模板新增浏览器类型严格限制功能

--- magic_dash/templates/magic-dash-pro/configs/base_config.py
+++ magic_dash/templates/magic-dash-pro/configs/base_config.py
@@ -10,12 +10,19 @@ class BaseConfig:
     # 应用版本
     app_version: str = "0.2.6"
 
+    # 应用密钥
+    app_secret_key: str = "magic-dash-pro-demo"
+
     # 浏览器最低版本限制规则
     min_browser_versions: List[dict] = [
         {"browser": "Chrome", "version": 88},
         {"browser": "Firefox", "version": 78},
         {"browser": "Edge", "version": 100},
     ]
+
+    # 是否基于min_browser_versions开启严格的浏览器类型限制
+    # 不在min_browser_versions规则内的浏览器将被直接拦截
+    strict_browser_type_check: bool = False
 
     # 是否启用重复登录辅助检查
     enable_duplicate_login_check: bool = True
--- magic_dash/templates/magic-dash-pro/server.py
+++ magic_dash/templates/magic-dash-pro/server.py
@@ -18,7 +18,7 @@ app = dash.Dash(
 server = app.server
 
 # 设置应用密钥
-app.server.secret_key = "magic-dash-pro-demo"
+app.server.secret_key = BaseConfig.app_secret_key
 
 # 为当前应用添加flask-login用户登录管理
 login_manager = LoginManager()
@@ -90,7 +90,7 @@ def check_browser():
         # IE相关浏览器直接拦截
         if user_agent.browser.family == "IE":
             return (
-                "<div style='font-size: 22px; color: red; position: fixed; top: 40%; left: 50%; transform: translateX(-50%);'>"
+                "<div style='font-size: 16px; color: red; position: fixed; top: 40%; left: 50%; transform: translateX(-50%);'>"
                 "请不要使用IE浏览器，或开启了IE内核兼容模式的其他浏览器访问本应用</div>"
             )
         # 基于BaseConfig.min_browser_versions配置，对相关浏览器最低版本进行检查
@@ -101,6 +101,21 @@ def check_browser():
                 and user_agent.browser.version[0] < rule["version"]
             ):
                 return (
-                    "<div style='font-size: 22px; color: red; position: fixed; top: 40%; left: 50%; transform: translateX(-50%);'>"
+                    "<div style='font-size: 16px; color: red; position: fixed; top: 40%; left: 50%; transform: translateX(-50%);'>"
                     "您的{}浏览器版本低于本应用最低支持版本（{}），请升级浏览器后再访问</div>"
                 ).format(rule["browser"], rule["version"])
+
+        # 若开启了严格的浏览器类型限制
+        if BaseConfig.strict_browser_type_check:
+            # 若当前浏览器不在声明的浏览器范围内
+            if user_agent.browser.family not in [
+                rule["browser"] for rule in BaseConfig.min_browser_versions
+            ]:
+                return (
+                    "<div style='font-size: 16px; color: red; position: fixed; top: 40%; left: 50%; transform: translateX(-50%);'>"
+                    "当前浏览器类型不在支持的范围内，支持的浏览器类型有：{}</div>"
+                ).format(
+                    "、".join(
+                        [rule["browser"] for rule in BaseConfig.min_browser_versions]
+                    )
+                )
