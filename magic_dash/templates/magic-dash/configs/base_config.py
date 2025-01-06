1273eb568b5b3cb891c443fc8b3829b8148d130a
CNFeffery <fefferypzy@gmail.com>
2025-01-06 16:10:29 +0800

    feat: magic-dash模板新增严格的浏览器类型限制功能

--- magic_dash/templates/magic-dash/configs/base_config.py
+++ magic_dash/templates/magic-dash/configs/base_config.py
@@ -16,3 +16,7 @@ class BaseConfig:
         {"browser": "Firefox", "version": 78},
         {"browser": "Edge", "version": 100},
     ]
+
+    # 是否基于min_browser_versions开启严格的浏览器类型限制
+    # 不在min_browser_versions规则内的浏览器将被直接拦截
+    strict_browser_type_check: bool = False
