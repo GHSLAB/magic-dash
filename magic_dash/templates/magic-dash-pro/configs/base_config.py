a749ef79b6bb64953ae8ebb8a38c73d2e387d86a
CNFeffery <fefferypzy@gmail.com>
2025-03-22 23:12:26 +0800

    chore: 更新下一版本号

--- magic_dash/templates/magic-dash-pro/configs/base_config.py
+++ magic_dash/templates/magic-dash-pro/configs/base_config.py
@@ -8,7 +8,7 @@ class BaseConfig:
     app_title: str = "Magic Dash Pro"
 
     # 应用版本
-    app_version: str = "0.2.9"
+    app_version: str = "0.3.0"
 
     # 应用密钥
     app_secret_key: str = "magic-dash-pro-demo"
