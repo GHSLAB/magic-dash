6d7e87cfc8a6f24110bca6aac68f45e5b6c5f78f
CNFeffery <fefferypzy@gmail.com>
2025-04-16 10:09:20 +0800

    chore: magic-dash模板新增依赖版本强制检查功能

--- magic_dash/templates/magic-dash/app.py
+++ magic_dash/templates/magic-dash/app.py
@@ -9,6 +9,22 @@ from server import app
 from configs import RouterConfig  # 路由配置参数
 from views.status_pages import _404, _500  # 各状态页面
 from views import core_pages
+from feffery_dash_utils.version_utils import (
+    check_python_version,
+    check_dependencies_version,
+)
+
+# 检查Python版本
+check_python_version(min_version="3.8", max_version="3.12")
+# 检查关键依赖库版本
+check_dependencies_version(
+    rules=[
+        {"name": "dash", "specifier": "==2.18.2"},
+        {"name": "feffery_antd_components", "specifier": "==0.3.15"},
+        {"name": "feffery_utils_components", "specifier": "==0.2.0rc27"},
+        {"name": "feffery_dash_utils", "specifier": ">=0.2.4"},
+    ]
+)
 
 app.layout = lambda: fuc.FefferyTopProgress(
     [
