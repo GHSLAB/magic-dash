77d5a5e17d166d10885271e338eede91afc7013d
CNFeffery <fefferypzy@gmail.com>
2025-07-25 10:03:39 +0800

    refactor: 内置simple-tool项目模板适配Dash 3.x

--- magic_dash/templates/simple-tool/app.py
+++ magic_dash/templates/simple-tool/app.py
@@ -10,14 +10,14 @@ from feffery_dash_utils.version_utils import (
 )
 
 # 检查Python版本
-check_python_version(min_version="3.8", max_version="3.12")
+check_python_version(min_version="3.8", max_version="3.13")
 # 检查关键依赖库版本
 check_dependencies_version(
     rules=[
-        {"name": "dash", "specifier": "==2.18.2"},
-        {"name": "feffery_antd_components", "specifier": "==0.3.15"},
-        {"name": "feffery_utils_components", "specifier": "==0.2.0rc27"},
-        {"name": "feffery_dash_utils", "specifier": ">=0.2.4"},
+        {"name": "dash", "specifier": ">=3.1.1,<4.0.0"},
+        {"name": "feffery_antd_components", "specifier": ">=0.4.0,<0.5.0"},
+        {"name": "feffery_utils_components", "specifier": ">=0.3.0,<0.4.0"},
+        {"name": "feffery_dash_utils", "specifier": ">=0.2.6"},
     ]
 )
 
